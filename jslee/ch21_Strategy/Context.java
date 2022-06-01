dc63433ab3674bb3eeafe98e3ec71cd72aa9403d
jslee314 <jslee_314@naver.com>
2022-06-01 20:36:34 +0900

    init commit

--- jslee/ch21_Strategy/Context.java
+++ jslee/ch21_Strategy/Context.java
@@ -1,4 +1,5 @@
 
+// Context를 사용하여 전략을 변경할 때 행동의 변화를 확인하십시오.
 public class Context {
     private Strategy strategy;
 
--- jslee/ch21_Strategy/StrategyPatternDemo.java
+++ jslee/ch21_Strategy/StrategyPatternDemo.java
@@ -1,4 +1,10 @@
-
+/**
+ * 로직이 변경되었을 때  유연하게 수정할 수 있습니다.
+ *
+ * operation방식이 변경되었을때 context에 들어가는
+ *
+ *
+ */
 public class StrategyPatternDemo {
     public static void main(String[] args) {
         Context context = new Context(new OperationAdd());
